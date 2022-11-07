cca6bafd8f60244e34bccebfdcd0a0ecd08af03a
orhanbspnr <88325672+orhanbspnr@users.noreply.github.com>
2022-11-07 16:32:23 +0300

    Add files via upload

---
+++ AsalSayi/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++) {
+            boolean isAsal = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isAsal = false;
+                }
+            }
+            if (isAsal) {
+                System.out.println(i);
+            }
+        }
+    }
+}
