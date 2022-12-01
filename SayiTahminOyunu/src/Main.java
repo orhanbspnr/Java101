361f96e3b0246ba79cae4df9f464e5c0b921f3f1
orhanbspnr <88325672+orhanbspnr@users.noreply.github.com>
2022-12-01 12:55:30 +0300

    Add files via upload

---
+++ SayiTahminOyunu/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int number = rand.nextInt(100);
+        Scanner input = new Scanner(System.in);
+        int right = 0;
+        boolean isWrong = false;
+        boolean isWin = false;
+        int[] wrong = new int[5];
+        System.out.println(number);
+        while (right < 5) {
+            System.out.print("Lütfen tahmininizi giriniz : ");
+            int selected = input.nextInt();
+            if (selected < 0 || selected > 99) {
+                System.out.println("Lütfen 0-100 arasında bir değer giriniz.");
+                if (isWrong) {
+                    right++;
+                    System.out.println("Çok fazla hatalı giriş yaptınız. Kalan hak : " + (5 - right));
+                } else {
+                    isWrong = true;
+                    System.out.println("Bir daha hatalı girişinizde hakkınızdan düşülecektir.");
+                }
+                continue;
+
+            }
+            if (selected == number) {
+                System.out.println("Tebrikler, doğru tahmin ! Tahmin ettiğini sayı : " + number);
+                isWin = true;
+                break;
+            } else {
+                System.out.println("Hatalı bir sayı girdiniz ! ");
+                if (selected > number) {
+                    System.out.println(selected + " sayısı, gizli sayıdan büyüktür");
+                } else {
+                    System.out.println(selected + " sayısı gizli sayıdan küçüktür.");
+                }
+                wrong[right++] = selected;
+                System.out.println("Kalan hak : " + (5 - right));
+            }
+
+        }
+
+        if (!isWin) {
+            System.out.println("Kaybettiniz ! ");
+            if (!isWrong) {
+                System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
+            }
+        }
+    }
+}
